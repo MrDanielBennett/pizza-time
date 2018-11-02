5f14aec8f4dcde029e715974c8dbbb06844fb345
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-11-02 10:26:41 -0700

    add html and link it with js as well as fix 'findPrice' function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 //backend
+
 function Pizza (size, cheese, topping) {
   this.size = size,
   this.cheese = cheese,
@@ -7,24 +8,25 @@ function Pizza (size, cheese, topping) {
 }
 
 Pizza.prototype.findPrice = function(){
-  if (this.size === small){
+  if (this.size === "small"){
     this.price = 5
-  } else if (this.size === medium){
-    this.price = 7.50
-  } else if (this.size === large){
+  } else if (this.size === "medium"){
+    this.price = 7.5
+  } else if (this.size === "large"){
     this.price = 10
   }
 }
 
 //frontend
 $(document).ready(function(){
-  $("#").submite(function(event){
+  $("#pizza-maker").submit(function(event){
     event.preventDefault();
-    var selectedSize
-    var selectedCheese
-    var selectedTopping
+    var selectedSize = $("#size").val();
+    var selectedCheese =$("#cheese").val();
+    var selectedTopping = $("#topping").val();
     newPizza = new Pizza (selectedSize, selectedCheese, selectedTopping);
     newPizza.findPrice();
+    console.log(newPizza)
     $("#result").text();
   })
 })
