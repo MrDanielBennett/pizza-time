a55f942e25481928ab3479114c23a3985bba6a4d
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-11-02 09:27:45 -0700

    add frontend javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
-function Pizza (size, cheese, topping, price) {
+//backend
+function Pizza (size, cheese, topping) {
   this.size = size,
   this.cheese = cheese,
   this.topping = topping,
   this.price = 5
 }
+
+Pizza.prototype.findPrice = function(){
+  if (this.size === small){
+    this.price = 5
+  } else if (this.size === medium){
+    this.price = 7.50
+  } else if (this.size === large){
+    this.price = 10
+  }
+}
+
+//frontend
+$(document).ready(function(){
+  $("#").submite(function(event){
+    event.preventDefault();
+    var selectedSize
+    var selectedCheese
+    var selectedTopping
+    newPizza = new Pizza (selectedSize, selectedCheese, selectedTopping);
+    newPizza.findPrice();
+    $("#result").text();
+  })
+})
